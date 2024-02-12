bce5e94c335611b2093db97920578aa6fa00da5e
rezzadtyp <rezzadityap@gmail.com>
2024-02-13 01:50:16 +0700

    day5

--- day5-exercise.js
+++ day5-exercise.js
@@ -268,3 +268,74 @@
 // console.log(playGame("scissors"));
 // console.log(playGame("rock"))
 
+// CARA 2
+
+// let score = {wins: 0, losses: 0, ties: 0};
+// function playGame(playerMove) {
+//   const computerMove = pickComputerMove();
+
+//   let result = '';
+
+//   if (playerMove === 'scissors') {
+//     if (computerMove === 'rock') {
+//       result = 'Lose.';
+//     } else if (computerMove === 'paper') {
+//       result = 'Win.';
+//     } else if (computerMove === 'scissors') {
+//       result = 'Tie.';
+//     }
+
+//   } else if (playerMove === 'paper') {
+//     if (computerMove === 'rock') {
+//       result = 'Win.';
+//     } else if (computerMove === 'paper') {
+//       result = 'Tie.';
+//     } else if (computerMove === 'scissors') {
+//       result = 'Lose.';
+//     }
+    
+//   } else if (playerMove === 'rock') {
+//     if (computerMove === 'rock') {
+//       result = 'Tie.';
+//     } else if (computerMove === 'paper') {
+//       result = 'Lose.';
+//     } else if (computerMove === 'scissors') {
+//       result = 'Win.';
+//     }
+
+//   }
+//   updateScore(result);
+//   return result;
+  
+// }
+
+// function pickComputerMove() {
+//   const randomNumber = Math.random();
+
+//   let computerMove = '';
+
+//   if (randomNumber >= 0 && randomNumber < 1 / 3) {
+//     computerMove = 'rock';
+//   } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+//     computerMove = 'paper';
+//   } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+//     computerMove = 'scissors';
+//   }
+
+//   return computerMove;
+// }
+
+// function updateScore(result) {
+//   if (result === 'Win.') {
+//     score.wins += 1;
+//   } else if (result === 'Lose.') {
+//     score.losses += 1;
+//   } else if (result === 'Tie.') {
+//     score.ties += 1;
+//   }
+// }
+
+// console.log(playGame("rock"));
+// console.log(playGame("rock"));
+// console.log(playGame("rock"));
+// console.log(playGame("rock"));
